acfc05322745f40fced9f568c53f7d4b46d24d94
kebishaa <kbkibatu@gmail.com>
2022-09-23 09:04:52 +0300

    added init fun

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -3,3 +3,9 @@ from logger import Logger
 import sys
 from datetime import datetime
 class DataExtractor():
+    def __init__(self)->None:
+        try:
+            self.logger=Logger().get_app_logger()
+            self.logger.info('Data extractor object Initialized')
+        except:
+            pass
