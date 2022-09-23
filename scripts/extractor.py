86f3fb85dba97deec6e9fe10b55227dfc7cabb03
kebishaa <kbkibatu@gmail.com>
2022-09-23 09:06:59 +0300

    added get_column and row fun

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -9,3 +9,19 @@ class DataExtractor():
             self.logger.info('Data extractor object Initialized')
         except:
             pass
+    def get_columns_and_rows(self,file_path)->tuple:
+        try:
+            with open(f'../data/{file_path}','r') as f:
+                lines=f.readlines()
+
+            columns=lines[0].replace('\n','').split(';')
+            data=lines[1:]
+            return columns,data
+        except Exception as e:
+            # the try excepts here are for the airflow
+            try:
+                self.logger.error(f"Failed to read data: {e}")
+            except:
+                pass
+            sys.exit(1)
+    
